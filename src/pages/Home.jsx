d1ca5d1b2b8d5e0aecb81298aa6cc0a421b9dde2
shaker11Batch <saenterprise67811@gmail.com>
2025-05-31 20:54:37 +0600

    hot

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../components/Banner';
+import HotJobs from './HotJobs';
 
 const Home = () => {
+    const [jobs, setJobs] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/jobs')
+            .then(res => res.json())
+        .then(data => setJobs(data))
+    },[])
+
     return (
         <div>
-            <Banner/>
+            <Banner />
+            <HotJobs jobs={jobs} setJobs={setJobs} />
         </div>
     );
 };
---
+++ src/pages/HotJobs.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import JobCard from './Shared/JobCard';
+
+
+const HotJobs = ({ jobs }) => {
+
+    console.log(jobs)
+    return (
+        <div>
+            <h2 className="text-4xl text-center my-6">Hot Jobs of the Day</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4'>
+                {
+                    jobs.map(job => <JobCard key={job._id} job={job}></JobCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default HotJobs;
